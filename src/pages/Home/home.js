d78ea8b584ae8ee1c34fa6bbbd66b683cbca17a2
victor.luna <victor.luna@hibrido.com.br>
2022-11-04 20:07:34 -0300

    feat(HOME): header criado e pg. home iniciada

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,16 +1,24 @@
 import React from "react";
-import styles from "./styles.scss"
+import Header from "../partials/header";
+import styles from "./styles.module.scss"
 
 function Home() {
   return (
-    <div className="home">
-      <h1 className="homeTitle">
-        Título home
-      </h1>
-
-      <p className="homeParagraph">
-        parágrafo na home
-      </p>
+    <div className={styles.home}>
+      <Header />         
+      <section className={styles.section1}>
+        <div className={styles.modalVermelho}>
+          <div className={styles.modalVermelhoConteudo}>
+            <p className={styles.modalVermelhoParagrafo}>Inscrições abertas para</p>
+            <div className={styles.modalVermelhoDivider}>
+              <p className={styles.modalVermelhoParagrafo2}>Mediadores Voluntários</p>
+              <p className={styles.modalVermelhoParagrafo3}>ASTEPI</p>
+            </div>
+            <p className={styles.modalVermelhoParagrafo4}>Contato: 2119-4157</p>
+            <p className={styles.modalVermelhoLogoUnicap}>LOGO UNICAP</p>
+          </div>
+        </div>
+      </section>
 
     </div>
   )
---
+++ src/pages/partials/header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from '../../assets/img/logo-normal.jpg'
+import styles from "./styles.module.scss"
+
+function Header() {
+  return (
+    <nav className={styles.header}>
+      <ul className={styles.headerItems} style={{"display": "flex", "flexDirection": "row"}}>
+        <li className={styles.navItem}>
+          <a className={styles.navLink} href="/">Home</a>
+        </li>
+
+        <li className={styles.navItem}>
+          <a className={styles.navLink} href="/conheca-a-astepi">CONHEÇA A ASTEPI</a>
+        </li>
+
+        <li className={styles.navItem}>
+          <a className={styles.navLink} href="/noticias">NOTÍCIAS</a>
+        </li>
+
+        <li className={styles.navItem}>
+          <a className={styles.navLink} href="/atendimento">COMO RECEBER ATENDIMENTO?</a>
+        </li>
+
+        <li className={styles.navItem}>
+          <a className={styles.navLink} href="/login">LOGIN</a>
+        </li>
+
+        <li className={styles.navItem}>
+          <a className={styles.navLink} href="/acesso-rapido">ACESSO RÁPIDO</a>
+        </li>
+
+        <li className={styles.navItem}>
+          <img alt="logo" src={logo} className={styles.headerLogo}  width="100" height="100" />
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header;
