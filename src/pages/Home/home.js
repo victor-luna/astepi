52f0f23dc503d0682316741949c6eaa029ad7d57
victor.luna <victor.luna@hibrido.com.br>
2022-11-06 11:37:16 -0300

    feat(HOME): section 2 criada

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,7 +5,8 @@ import styles from "./styles.module.scss"
 function Home() {
   return (
     <div className={styles.home}>
-      <Header />         
+      <Header />
+
       <section className={styles.section1}>
         <div className={styles.modalVermelho}>
           <div className={styles.modalVermelhoConteudo}>
@@ -18,6 +19,32 @@ function Home() {
             <p className={styles.modalVermelhoLogoUnicap}>LOGO UNICAP</p>
           </div>
         </div>
+      </section>
+
+      <section className={styles.section2}>
+
+        <div className={styles.quemSomos}>
+
+          <div className={styles.quemSomosTitulo}>
+            <p className={styles.quemSomosT1}>
+              Conheça
+            </p>
+            <h1 className={styles.quemSomosT2}>
+              ASTEPI
+            </h1>
+            <p className={styles.quemSomosT3}>
+              ASSESSORIA DE TREINAMENTO, ESTÁGIO, PESQUISA E INTEGRAÇÃO
+            </p>
+          </div>
+
+          <div className={styles.quemSomosDescricao}>
+            <div className={styles.quemSomosLinhaInicio}></div>
+            <div className={styles.quemSomosConteudo}>A ASTEPI do Núcleo de Prática Jurídica, criada em 30 de março de 1974, vem desenvolvendo atividades de ensino, pesquisa e extensão, cumprindo seus objetivos institucionais de promoção do conhecimento e da prática do exercício das funções jurídicas, prestação de servições de assistência jurídica gratuita à população carente e de integração do aluno à comunidade, visando, acima de tudo, à promoção da justiça social.</div>
+            <div className={styles.quemSomosLinhaFinal}></div>
+          </div>
+
+        </div>
+
       </section>
 
     </div>
