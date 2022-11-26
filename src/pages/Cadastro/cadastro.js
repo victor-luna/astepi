13227e1dcea069a2ab0bf1fe6bb601e9ad893372
João Guilherme <82194448+gbarros26@users.noreply.github.com>
2022-11-25 22:12:28 -0300

    testando cadastro

--- src/pages/Cadastro/cadastro.js
+++ src/pages/Cadastro/cadastro.js
@@ -15,59 +15,85 @@ return(
     
 <div className={styles.container}>
 
+<nav className={styles.header}>
+
+
 <li className={styles.navItem}>
 <img alt="logo" src={logo} className={styles.headerLogo}  width="100" height="100" />
 </li>
 
+
 <h1 className={styles.h1}>Cadastro de acesso</h1>
+
+</nav>
+
 
 
 <p className={styles.p}>Olá usuário!<br></br> Para conseguir acessar o sistema para agendamento, é necessario preencher as informações abaixo.</p>
 
 
 
-<div className={styles.linha1} />
+<div className={styles.blocoprincipal}>
 
-<div className={styles.linha2} />
+
 
 <div className={styles.primeirobloco}>
 
 <div className={styles.inputnome}>
     Nome<br></br>
-<input type="user" placeholder='' />
+<input type="user" className={styles.input} placeholder='' />
 <br></br>
 </div>
 
 <div className={styles.inputsobrenome}>
 Sobrenome<br></br>
-<input type="user" placeholder='' />
+<input type="user"  className={styles.input} placeholder='' />
 <br></br>
 </div>
 
-</div>
 
 
-
-<div className={styles.segundobloco}>
 
 <div className={styles.inputemail}>
 E-mail<br></br>  
-<input type="email" placeholder='' />
+<input type="email"  className={styles.input} placeholder='' />
 <br></br>
 </div>
 
 <div className={styles.inputpass}>
 Senha<br></br>
-<input type="password" placeholder='' />
+<input type="password"  className={styles.input} placeholder='' />
 <br></br>
 </div>
 
 <div className={styles.inputconpass}>
 Confirmar senha<br></br>
-<input type="password" placeholder='' />
+<input type="password"  className={styles.input} placeholder='' />
 <br></br>
 </div>
 
+
+<button  className={styles.button} onClick={(() => navigate("/Agendamento"))} >Cadastrar</button>
+
+
+</div>
+
+
+
+
+<div className={styles.segundobloco}>
+
+<div className={styles.inputcpf}>
+CPF<br></br>
+<input type="number"  className={styles.input} placeholder='' />
+<br></br>
+</div>
+
+<div className={styles.inputrg}>
+RG<br></br>
+<input type="number"  className={styles.input} placeholder='' />
+<br></br>
+</div>
 
 </div>
 
@@ -75,40 +101,21 @@ Confirmar senha<br></br>
 
 <div className={styles.terceirobloco}>
 
-<div className={styles.inputcpf}>
-CPF<br></br>
-<input type="number" placeholder='' />
-<br></br>
-</div>
-
-<div className={styles.inputrg}>
-RG<br></br>
-<input type="number" placeholder='' />
-<br></br>
-</div>
-
-</div>
-
-
-
-<div className={styles.quartobloco}>
-
 <div className={styles.inputendereco}>
 Endereço<br></br>
-<input type="text" placeholder='' />
+<input type="text"  className={styles.input} placeholder='' />
 <br></br>
 </div>
 
 <div className={styles.inputcep}>
 CEP<br></br>
-<input type="number" placeholder='' />
+<input type="number"  className={styles.input} placeholder='' />
 </div>
 
 </div>
 
 
-
-<button  className={styles.button} onClick={(() => navigate("/Agendamento"))} >Cadastrar</button>
+</div>
 
 
 </div>
