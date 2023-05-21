9830af00e7df9049da96c880ca0eab7c02540db5
Victor Luna <vitor.vazguimaraes@gmail.com>
2023-05-21 17:01:11 -0300

    feature/CRUD created to manage schedules

--- src/pages/Admin/partials/Content/CalendarContent/CalendarContent.jsx
+++ src/pages/Admin/partials/Content/CalendarContent/CalendarContent.jsx
@@ -13,6 +13,19 @@ const CalendarContent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [fetchedSchedules, setFetchedSchedules] = useState([]);
   const [isOpenSchedules, setIsOpenSchedules] = useState(false);
+  const [selectedSchedule, setSelectedSchedule] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchCurrentNewSchedule, setSearchCurrentNewSchedule] =
+    useState(false);
+  const [newSchedule, setNewSchedule] = useState({
+    userId: "",
+    ano: "",
+    dia: "",
+    horario: "",
+    mes: "",
+    observacao: "",
+  });
 
   useEffect(() => {
     const getCalendarDays = () => {
@@ -87,17 +100,23 @@ const CalendarContent = () => {
 
       const filteredUserNames = userNames.filter(Boolean);
       setFetchedUserNames(filteredUserNames);
+      console.log(usersResponses, "usersResponses");
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  const handleOpenModal = () => {
+  const handleOpenModal = (schedule) => {
+    setSelectedSchedule(schedule);
     setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
+  };
+
+  const handleCloseNewSchedule = () => {
+    setSearchCurrentNewSchedule(false);
   };
 
   const handleOpenSchedules = async () => {
@@ -140,6 +159,149 @@ const CalendarContent = () => {
       } catch (error) {
         console.error("Erro ao buscar os agendamentos: ", error);
       }
+    }
+  };
+
+  const handleSaveSchedule = async () => {
+    try {
+      const { id, dia, mes, ano, horario, observacao } = selectedSchedule;
+
+      // Make the PUT request to update the schedule
+      await axios.put(
+        `https://astepi-unicap.herokuapp.com/agendamentos/${id}`,
+        {
+          dia,
+          mes,
+          ano,
+          horario,
+          observacao,
+        }
+      );
+
+      // Update the necessary state variables
+      // Close the modal, reset form fields, or perform any other required actions
+    } catch (error) {
+      console.error("Error updating schedule:", error);
+    }
+  };
+
+  const handleDeleteSchedule = async () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this schedule?"
+    );
+
+    if (!confirmed) {
+      return;
+    }
+
+    try {
+      const { id } = selectedSchedule;
+
+      await axios.delete(
+        `https://astepi-unicap.herokuapp.com/agendamentos/${id}`
+      );
+
+      setFetchedSchedules((prevSchedules) =>
+        prevSchedules.filter((schedule) => schedule.id !== id)
+      );
+
+      handleCloseModal();
+    } catch (error) {
+      console.error("Error deleting schedule:", error);
+    }
+  };
+
+  const handleSearch = async () => {
+    if (searchQuery.length < 3) {
+      window.confirm("Digite pelo menos 3 (três) caractéres.");
+      setSearchQuery("");
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        `https://astepi-unicap.herokuapp.com/usuarios`
+      );
+
+      const matchingUsers = response.data.content;
+
+      const filteredUsers = matchingUsers.filter((user) => {
+        const name = user.nome.toLowerCase();
+        const id = user.id.toLowerCase();
+        const query = searchQuery.toLowerCase();
+
+        return name.includes(query) || id.includes(query);
+      });
+
+      setSearchResult(filteredUsers);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const SearchResult = ({ users }) => {
+    return (
+      <div>
+        <h4>Matching Users:</h4>
+        {users.length > 0 ? (
+          <ul className={styles.userList}>
+            {users.map((user, index) => (
+              <div className={styles.schedule} key={index}>
+                <p>{user.nome}</p>
+                {user.agendamentos.map((schedule, scheduleIndex) => (
+                  <div key={scheduleIndex}>
+                    <p>
+                      {schedule.horario} ({schedule.dia}/{schedule.mes}/
+                      {schedule.ano})
+                    </p>
+                    <p>{schedule.observacao}</p>
+                    <button onClick={() => handleOpenModal(schedule)}>
+                      Edit Schedule
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <p>No matching users found!</p>
+        )}
+      </div>
+    );
+  };
+
+  const handleOpenNewScheduleModal = () => {
+    setSearchCurrentNewSchedule((prevState) => !prevState);
+  };
+
+  const handleCreateSchedule = async (event, newSchedule) => {
+    event.preventDefault();
+
+    try {
+      const { userId, ...payload } = newSchedule;
+      // Make the POST request to create a new schedule
+      const response = await axios.post(
+        `https://astepi-unicap.herokuapp.com/usuarios/${newSchedule.userId}/agendamentos`,
+        payload
+      );
+
+      const createdSchedule = response.data;
+
+      setFetchedSchedules((prevSchedules) => [
+        ...prevSchedules,
+        createdSchedule,
+      ]);
+      setSearchCurrentNewSchedule(false);
+      setNewSchedule({
+        userId: "",
+        ano: "",
+        dia: "",
+        horario: "",
+        mes: "",
+        observacao: "",
+      });
+    } catch (error) {
+      console.error("Error creating schedule:", error);
     }
   };
 
@@ -246,9 +408,113 @@ const CalendarContent = () => {
       </div>
 
       <div className={styles.crudMenu}>
-        <button onClick={handleOpenSchedules}>
-          {isOpenSchedules ? "Fechar agendamentos" : "Buscar agendamentos"}
-        </button>
+        <div
+          className={styles.searchContainer}
+          style={{ display: "flex", flexDirection: "row", gap: "1rem" }}
+        >
+          <div className={styles.search}>
+            <input
+              type="text"
+              placeholder="Search by name or ID"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+          <button onClick={handleOpenSchedules}>
+            {isOpenSchedules ? "Fechar agendamentos" : "Buscar agendamentos"}
+          </button>
+          <button onClick={() => handleOpenNewScheduleModal()}>Create</button>
+        </div>
+        {searchResult.length > 0 && <SearchResult users={searchResult} />}
+
+        {searchCurrentNewSchedule && (
+          <div className={styles.modal}>
+            <div className={styles.modalContent}>
+              <button
+                className={styles.closeButton}
+                onClick={handleCloseNewSchedule}
+              >
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+
+              <form>
+                <input
+                  type="text"
+                  value={newSchedule?.userId}
+                  onChange={(e) =>
+                    setNewSchedule({
+                      ...newSchedule,
+                      userId: e.target.value,
+                    })
+                  }
+                  placeholder="User Id"
+                />
+                <input
+                  type="text"
+                  value={newSchedule?.ano}
+                  onChange={(e) =>
+                    setNewSchedule({
+                      ...newSchedule,
+                      ano: e.target.value,
+                    })
+                  }
+                  placeholder="Ano"
+                />
+                <input
+                  type="text"
+                  value={newSchedule?.dia}
+                  onChange={(e) =>
+                    setNewSchedule({
+                      ...newSchedule,
+                      dia: e.target.value,
+                    })
+                  }
+                  placeholder="Dia"
+                />
+                <input
+                  type="text"
+                  value={newSchedule?.horario}
+                  onChange={(e) =>
+                    setNewSchedule({
+                      ...newSchedule,
+                      horario: e.target.value,
+                    })
+                  }
+                  placeholder="Horario"
+                />
+                <input
+                  type="text"
+                  value={newSchedule?.mes}
+                  onChange={(e) =>
+                    setNewSchedule({
+                      ...newSchedule,
+                      mes: e.target.value,
+                    })
+                  }
+                  placeholder="Mês"
+                />
+                <input
+                  type="text"
+                  value={newSchedule?.observacao}
+                  onChange={(e) =>
+                    setNewSchedule({
+                      ...newSchedule,
+                      observacao: e.target.value,
+                    })
+                  }
+                  placeholder="Observação"
+                />
+
+                <button
+                  onClick={(event) => handleCreateSchedule(event, newSchedule)}
+                >
+                  Create
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
 
         {isOpenSchedules && (
           <>
@@ -260,20 +526,85 @@ const CalendarContent = () => {
                   {schedule.ano})
                 </p>
                 <p>{schedule.observacao}</p>
-                <button onClick={handleOpenModal}>Edit Schedule</button>
+                <button onClick={() => handleOpenModal(schedule)}>
+                  Edit Schedule
+                </button>
               </div>
             ))}
           </>
         )}
-
         {isModalOpen && (
           <div className={styles.modal}>
             <div className={styles.modalContent}>
               <button className={styles.closeButton} onClick={handleCloseModal}>
                 <FontAwesomeIcon icon={faTimes} />
               </button>
-              <h2>Conteúdo do modal CRUD</h2>
-              {/* Add more schedule divs as needed */}
+              {<h2>Agendamento de {selectedSchedule.usuario}</h2>}
+              {console.log(selectedSchedule, "selectedSchedule")}
+
+              <form>
+                <input
+                  type="text"
+                  value={selectedSchedule.horario}
+                  onChange={(e) =>
+                    setSelectedSchedule({
+                      ...selectedSchedule,
+                      horario: e.target.value,
+                    })
+                  }
+                />
+                <input
+                  type="text"
+                  value={selectedSchedule.dia}
+                  onChange={(e) =>
+                    setSelectedSchedule({
+                      ...selectedSchedule,
+                      dia: e.target.value,
+                    })
+                  }
+                />
+
+                <input
+                  type="text"
+                  value={selectedSchedule.mes}
+                  onChange={(e) =>
+                    setSelectedSchedule({
+                      ...selectedSchedule,
+                      mes: e.target.value,
+                    })
+                  }
+                />
+
+                <input
+                  type="text"
+                  value={selectedSchedule.ano}
+                  onChange={(e) =>
+                    setSelectedSchedule({
+                      ...selectedSchedule,
+                      ano: e.target.value,
+                    })
+                  }
+                />
+
+                <input
+                  type="text"
+                  value={selectedSchedule.observacao}
+                  onChange={(e) =>
+                    setSelectedSchedule({
+                      ...selectedSchedule,
+                      observacao: e.target.value,
+                    })
+                  }
+                />
+              </form>
+
+              {/* Save/Update Button */}
+              <button onClick={handleSaveSchedule}>Save/Update Schedule</button>
+
+              {/* Delete Button */}
+              <button onClick={() => handleDeleteSchedule()}>
+                Delete Schedule
+              </button>
             </div>
           </div>
         )}
