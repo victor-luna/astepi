4b46f90b508b5e0e03a8a54a1f13f2374b0fca30
João Guilherme <82194448+gbarros26@users.noreply.github.com>
2023-06-03 17:32:30 -0300

    change login

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,72 +1,64 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
-import logo from "../../assets/img/logo-normal.jpg";
+import LogoBranca from "../../assets/img/logo-branca-unicap.png";
+import backgroundlogin from "../../assets/img/backgroundlogin.png";
 
 function Login() {
   const navigate = useNavigate();
 
   return (
-   
     <>
-    <div className={styles.container}>
-        
-     
-      <div className={styles.container1}>
+      <div className={styles.container}>
+          <div className={styles.container1}>
+            <h2 className={styles.h2}>Login</h2>
 
-      <h2 className={styles.h2}>Login</h2>
+              <div className={styles.input}>
+                <label htmlFor="usuario">Digite seu Email ou CPF :</label> <br></br>
+                <input type="text" placeholder="Usuário" id="usuario" name="usuario" required className={styles.inputedit} />
+              </div>
 
-        <div className={styles.input}>
-          <label for="number">Digite seu Email ou CPF :</label>
-          <input type="user" placeholder="Usuário"  id="usuario" name="usuario"/> 
-        </div>
-      
-        <div className={styles.input}>
-          <label for="number">Digite sua senha :</label>
-          <input type="password" placeholder="Senha" id="senha" name="senha"/>
-        </div>
+                <div className={styles.input}>
+                  <label htmlFor="senha">Digite sua senha :</label> <br></br>
+                  <input type="password" placeholder="Senha" id="senha" name="senha" required className={styles.inputedit}/>
+                </div>
+                  
+              
+                  <div className={styles.divbuttom}>
 
-        <div className={styles.buttom}>
-       
-          <button onClick={() => navigate("/Agendamento")}>Acessar</button>
-        </div>
+                  <div className={styles.buttom}>
+                    <button onClick={() => navigate("/Agendamento")}>Acessar</button> 
+                  </div>
 
-        <div className={styles.buttom}>
-        
-        <button onClick={()=> navigate("/")}>Início</button>
-        </div>
+                    <div className={styles.buttom}>
+                      <button onClick={() => navigate("/")}>Início</button>
+                    </div>
+            
+                  </div>
 
-        <div className={styles.cadastro}>
-        <a href="/cadastro">Não possui cadastro?</a>
-        </div>
+                    <br></br>
+                    <div className={styles.cadastro}>
+                      <a href="/cadastro">Não possui cadastro?</a>
+                    </div>
+          </div>
 
+          <div className={styles.container2}>
+                <li className={styles.navItem}>
+                <img alt="logo" src={LogoBranca} width="150" height="150" />
+                </li>
+
+            
+          
+
+                  <h2 className={styles.h2}>Astepi</h2>
+                  <h2 className={styles.h2}>Assistencia Judiciária</h2>
+
+                    <img src={backgroundlogin} alt="backgroundlogin" className={styles.backgroundlogin}/>
+
+
+          </div>
       </div>
-    
-         <div className={styles.container2}>
-
-         <li className={styles.navItem}>
-            <img
-              alt="logo"
-              src={logo}
-              className={styles.headerLogo}
-              width="100"
-              height="100"
-            />
-          </li>
-
-            <h2 className={styles.h2}>Astepi</h2>
-            <h3 className={styles.h2}>Assistencia Judiciária</h3>
-         
-         <div className={styles.container3}></div>
-         
-         </div>
-    
-       </div>
-    
-  
-  
     </>
-    
   );
 }
 
